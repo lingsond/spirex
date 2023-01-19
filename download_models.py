d590dd11311cfc794e36d769b1780c94936006ca
lingson <dirk.lingson@gmail.com>
2023-01-19 21:37:48 +0100

    change docker

--- download_models.py
+++ download_models.py
@@ -7,11 +7,11 @@ if __name__ == '__main__':
     # Model Initialization
     PROMPT_MODEL = ['google/flan-t5-large']
     for model in PROMPT_MODEL:
-        PTOKENIZER = T5Tokenizer.from_pretrained(model, cache_dir="/.cache")
-        PMODEL = T5ForConditionalGeneration.from_pretrained(model, cache_dir="/.cache")
+        PTOKENIZER = T5Tokenizer.from_pretrained(model, cache_dir="/spirex/cache/")
+        PMODEL = T5ForConditionalGeneration.from_pretrained(model, cache_dir="/spirex/cache/")
     QA_MODEL = ['deepset/deberta-v3-large-squad2', 'deepset/roberta-large-squad2']
     for model in QA_MODEL:
-        QTOKENIZER = AutoTokenizer.from_pretrained(model, cache_dir="/.cache")
-        qmodel = AutoModelForQuestionAnswering.from_pretrained(model, cache_dir="/.cache")
+        QTOKENIZER = AutoTokenizer.from_pretrained(model, cache_dir="/spirex/cache")
+        qmodel = AutoModelForQuestionAnswering.from_pretrained(model, cache_dir="/spirex/cache")
     # QMODEL = pipeline("question-answering", model=qmodel, tokenizer=QTOKENIZER)
-    BERTSCORE = load("bertscore", cache_dir="/.cache")
+    BERTSCORE = load("bertscore", cache_dir="/spirex/cache")
