3b6505c338d076d5973cf0e908329a2403ff2823
lingson <dirk.lingson@gmail.com>
2023-01-19 18:48:50 +0100

    edit Dockerfile

--- download_models.py
+++ download_models.py
@@ -5,11 +5,13 @@ from evaluate import load
 
 if __name__ == '__main__':
     # Model Initialization
-    PROMPT_MODEL = 'google/flan-t5-large'
-    QA_MODEL = 'deepset/deberta-v3-large-squad2'
-    PTOKENIZER = T5Tokenizer.from_pretrained(PROMPT_MODEL, cache_dir="./cache")
-    PMODEL = T5ForConditionalGeneration.from_pretrained(PROMPT_MODEL, cache_dir="./cache")
-    QTOKENIZER = AutoTokenizer.from_pretrained(QA_MODEL, cache_dir="./cache")
-    qmodel = AutoModelForQuestionAnswering.from_pretrained(QA_MODEL, cache_dir="./cache")
-    QMODEL = pipeline("question-answering", model=qmodel, tokenizer=QTOKENIZER)
+    PROMPT_MODEL = ['google/flan-t5-large']
+    for model in PROMPT_MODEL:
+        PTOKENIZER = T5Tokenizer.from_pretrained(model, cache_dir="./cache")
+        PMODEL = T5ForConditionalGeneration.from_pretrained(model, cache_dir="./cache")
+    QA_MODEL = ['deepset/deberta-v3-large-squad2', 'deepset/roberta-large-squad2']
+    for model in QA_MODEL:
+        QTOKENIZER = AutoTokenizer.from_pretrained(model, cache_dir="./cache")
+        qmodel = AutoModelForQuestionAnswering.from_pretrained(model, cache_dir="./cache")
+    # QMODEL = pipeline("question-answering", model=qmodel, tokenizer=QTOKENIZER)
     BERTSCORE = load("bertscore", cache_dir="./cache")
