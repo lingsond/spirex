b256d641e6364c253ae51c9cda78212443b94df2
lingson <dirk.lingson@gmail.com>
2023-01-23 22:17:08 +0100

    new pipeline without bertscore

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM pytorch/pytorch:1.12.0-cuda11.3-cudnn8-runtime
 
 ENV TRANSFORMER_CACHE=/spirex/cache/
-RUN pip3 install docker transformers tqdm evaluate sentencepiece bert-score
+# RUN pip3 install docker transformers tqdm evaluate sentencepiece bert-score
+RUN pip3 install docker transformers tqdm sentencepiece sentence-transformers
 
 WORKDIR /spirex/
 COPY run_hybrid_deberta.py run_hybrid_roberta.py run_prompt.py input.jsonl /spirex/
@@ -16,4 +17,4 @@ RUN chmod -R 777 /.cache/
 
 ENTRYPOINT [ "/spirex/run_hybrid_deberta.py" ]
 
-# NAME=ls6-stud-registry.informatik.uni-wuerzburg.de/studwangsadirdja-spirex:0.0.1
+# NAME=ls6-stud-registry.informatik.uni-wuerzburg.de/studwangsadirdja-spirex:0.0.2
--- download_models.py
+++ download_models.py
@@ -3,7 +3,8 @@ from transformers import (
     AutoTokenizer, AutoModelForQuestionAnswering,
     AutoModelForMaskedLM
 )
-from evaluate import load
+# from evaluate import load
+from sentence_transformers import SentenceTransformer, util
 
 
 if __name__ == '__main__':
@@ -16,13 +17,14 @@ if __name__ == '__main__':
     for model in QA_MODEL:
         QTOKENIZER = AutoTokenizer.from_pretrained(model, cache_dir="/spirex/cache")
         qmodel = AutoModelForQuestionAnswering.from_pretrained(model, cache_dir="/spirex/cache")
-    SCORE_MODEL = ['distilbert-base-uncased']
-    for model in SCORE_MODEL:
-        STOKENIZER = AutoTokenizer.from_pretrained(model, cache_dir="/.cache/huggingface/hub/")
-        smodel = AutoModelForMaskedLM.from_pretrained(model, cache_dir="/spirex/cache")
+    #SCORE_MODEL = ['distilbert-base-uncased']
+    #for model in SCORE_MODEL:
+    #    STOKENIZER = AutoTokenizer.from_pretrained(model, cache_dir="/.cache/huggingface/hub/")
+    #    smodel = AutoModelForMaskedLM.from_pretrained(model, cache_dir="/spirex/cache")
     # QMODEL = pipeline("question-answering", model=qmodel, tokenizer=QTOKENIZER)
-    BERTSCORE = load("bertscore", cache_dir="/spirex/cache")
-    bscore = BERTSCORE.compute(
-        predictions=["Annakin Skywalker"], references=["Darth Vader"],
-        model_type="distilbert-base-uncased"
-    )
+    # BERTSCORE = load("bertscore", cache_dir="/spirex/cache")
+    #bscore = BERTSCORE.compute(
+    #    predictions=["Annakin Skywalker"], references=["Darth Vader"],
+    #    model_type="distilbert-base-uncased"
+    #)
+    embedder = SentenceTransformer('all-MiniLM-L6-v2', cache_folder="/spirex/cache")
